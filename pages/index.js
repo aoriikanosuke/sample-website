fefac2d9567c15f2e5f9811a89f37e66874ad57f
aoriikamosuke <aoriika1122@icloud.com>
2025-07-24 09:02:12 +0900

    add Creating a simple blog architecture

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,7 @@ export default function Home() {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>[Hello, my name is Aori Yamada. I'm 21. I like development of software. I going to office and development of it-company everyday. nice to meet you.]</p>
+        <p>Hello, my name is Aori Yamada. I'm 21. I like development of software. I going to office and development of it-company everyday. nice to meet you.</p>
         <p>
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
